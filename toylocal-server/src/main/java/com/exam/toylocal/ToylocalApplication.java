6ea6f7c7bd7f8efeb3a4ef579e11eea80e198cc2
함명선 <hms1475@gmail.com>
2018-11-12 17:56:08 +0900

    init.

---
+++ toylocal-server/src/main/java/com/exam/toylocal/ToylocalApplication.java
@@ -0,0 +1,12 @@
+package com.exam.toylocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToylocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToylocalApplication.class, args);
+    }
+}
